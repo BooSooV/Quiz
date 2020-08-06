95de1f419ff015e54c71c0783faca537c5196f0e
BooSooV <65161196+BooSooV@users.noreply.github.com>
2020-08-06 18:21:33 +0300

    Relokated files, removed tests, first stat (service work well)

--- QuizEngine/src/main/java/engine/Option.java
+++ QuizEngine/src/main/java/engine/Option.java
@@ -1,4 +1,4 @@
-package engine;
+package engine;//package engine;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
---
+++ QuizEngine/src/main/java/engine/QuizEngine.java
@@ -0,0 +1,11 @@
+package engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizEngine {
+    public static void main(String[] args) {
+        SpringApplication.run(QuizEngine.class, args);
+    }
+}
